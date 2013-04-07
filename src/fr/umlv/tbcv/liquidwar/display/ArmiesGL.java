2737ce567c43cc3dc4166d1a3ca64c6c4640cf01
Christophe Venevongsos <chris1228@gmail.com>
2013-04-07 20:44:22 +0200

    Added the ArmiesGL class as a wrapper for Armies for the drawing backend

---
+++ src/fr/umlv/tbcv/liquidwar/display/ArmiesGL.java
@@ -0,0 +1,114 @@
+package fr.umlv.tbcv.liquidwar.display;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
+
+import android.opengl.GLES20;
+
+import fr.umlv.tbcv.liquidwar.logic.Armies;
+
+public class ArmiesGL {
+	private IntBuffer vertexBuffer ;
+	private int mMVPMatrixHandle;
+	
+	// Number of coordinates per vertex in this array
+	static final int COORDS_PER_VERTEX = 2 ;
+	
+	private Armies armies ;
+	
+	//Color of the point
+	float color[] = { 0.44215f, 0.226953125f, 0.82265625f, 1.0f };
+	
+    private final String vertexShaderCode =
+            // This matrix member variable provides a hook to manipulate
+            // the coordinates of the objects that use this vertex shader
+            "uniform mat4 uMVPMatrix;" +
+
+            "attribute vec4 vPosition;" +
+            "void main() {" +
+            // the matrix must be included as a modifier of gl_Position
+            "  gl_Position = vPosition * uMVPMatrix;" +
+            "}";
+
+	private final String fragmentShaderCode =
+		    "precision mediump float;" +
+		    "uniform vec4 vColor;" +
+		    "void main() {" +
+		    "  gl_FragColor = vColor;" +
+		    "}";
+
+	private int mProgram;
+
+	private int mPositionHandle;
+
+	private int mColorHandle;
+	
+	private final int vertexCount = Armies.fighterNumber * 2 / COORDS_PER_VERTEX;
+    private final int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per vertex
+	
+	public ArmiesGL(Armies a) {
+		
+		armies = a ;
+		
+		// Allocating number of element in the array * 4 bytes ( Size of a float ) 
+		ByteBuffer bb = ByteBuffer.allocateDirect( Armies.fighterNumber * 2 * 4 ) ;
+		bb.order(ByteOrder.nativeOrder() ) ;
+		
+		vertexBuffer = bb.asIntBuffer() ;
+		vertexBuffer.put( armies.getFightersPosition() ) ;
+		vertexBuffer.position(0) ;
+		
+		 int vertexShader = LiquidWarRenderer.loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode);
+		 int fragmentShader = LiquidWarRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
+
+		 mProgram = GLES20.glCreateProgram();             // create empty OpenGL ES Program
+		 GLES20.glAttachShader(mProgram, vertexShader);   // add the vertex shader to program
+		 GLES20.glAttachShader(mProgram, fragmentShader); // add the fragment shader to program
+		 GLES20.glLinkProgram(mProgram);                  // creates OpenGL ES program executables
+		
+	}
+	
+	public void draw(float [] mvpMatrix ) {
+		
+		// Update the int array with the actual fighters positions
+		armies.retrieveFightersPosition() ;
+		
+		// Add program to OpenGL environment
+        GLES20.glUseProgram(mProgram);
+
+        // get handle to vertex shader's vPosition member
+        mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+
+        // Enable a handle to the triangle vertices
+        GLES20.glEnableVertexAttribArray(mPositionHandle);
+
+        // Prepare the triangle coordinate data
+        GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX,
+                                     GLES20.GL_INT, false,
+                                     vertexStride, vertexBuffer);
+
+        // get handle to fragment shader's vColor member
+        mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+
+        // Set color for drawing the triangle
+        GLES20.glUniform4fv(mColorHandle, 1, color, 0);
+
+        // get handle to shape's transformation matrix
+        mMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+        LiquidWarRenderer.checkGlError("glGetUniformLocation");
+
+        // Apply the projection and view transformation
+        GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mvpMatrix, 0);
+        LiquidWarRenderer.checkGlError("glUniformMatrix4fv");
+
+        // Draw the triangle
+        GLES20.glDrawArrays(GLES20.GL_POINTS, 0, vertexCount);
+
+        // Disable vertex array
+        GLES20.glDisableVertexAttribArray(mPositionHandle);
+	}
+
+	
+
+}
